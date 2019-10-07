08868d1a89bf3e0555cc7ca0ecea48966a07f07c
thor1824 <42994659+thor1824@users.noreply.github.com>
2019-10-07 14:41:05 +0200

    Update ClothingRepository.cs

--- ClothShop.Infrastructure/Repositories/ClothingRepository.cs
+++ ClothShop.Infrastructure/Repositories/ClothingRepository.cs
@@ -1,14 +1,23 @@
 ﻿using ClothShop.Core.DomainServices;
 using ClothShop.Core.Entity;
+using ClothShop.Infrastructure.Contexts;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ClothShop.Infrastructure.Repositories
 {
+    
 
     public class ClothingRepository : IRepository<ClothingArticle>
     {
+        private readonly ClothingContext _ctx;
+
+        public ClothingRepository(ClothingContext ctx)
+        {
+            _ctx = ctx;
+        }
+
         public ClothingArticle Create(ClothingArticle entity)
         {
             throw new NotImplementedException();
