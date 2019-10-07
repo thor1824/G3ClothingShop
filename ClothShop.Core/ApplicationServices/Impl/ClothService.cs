96cf196010d584c043be852659197c19ba4f8c17
Warfor <44639824+Warfor@users.noreply.github.com>
2019-10-07 11:42:49 +0200

    crud start

---
+++ ClothShop.Core/ApplicationServices/Impl/ClothService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClothShop.Core.DomainServices;
+using ClothShop.Core.Entity;
+
+namespace ClothShop.Core.ApplicationServices.Impl
+{
+    public class ClothService
+    {
+        public void Create(ClothingArticle CreateCloth)
+        {
+
+        }
+
+        public List<> ReadAll(ClothingArticle ReadAllCloth)
+        {
+            
+        }
+        public void Read (int id)
+        {
+
+        }
+
+        public void Update(ClothingArticle UpdateCloth)
+        {
+            
+        }
+
+        public void Delete(ClothingArticle DeleteCloth)
+        {
+
+        }
+    }
+
+}
